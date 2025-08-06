c893a41a79b132727fd285301bcac695a1576264
Val <val3nt1no35@gmail.com>
2025-08-06 01:43:12 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const spellMap = {};
+
+app.post("/cast", (req, res) => {
+  const { userId, spell } = req.body;
+  spellMap[userId] = spell;
+  console.log(`Received spell from ${userId}: ${spell}`);
+  res.sendStatus(200);
+});
+
+app.get("/get-spell/:userId", (req, res) => {
+  const spell = spellMap[req.params.userId] || null;
+  res.json({ spell });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Spell server running on port ${port}`));
